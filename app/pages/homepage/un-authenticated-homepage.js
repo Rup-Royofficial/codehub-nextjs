0f7bb5f6bf5fda26d4cf4aca72dddfaa710aec2f
Rup-Royofficial <rupayanroy981@gmail.com>
2023-11-29 01:20:08 +0530

    day3 changes, added gsap animation to header

--- app/pages/homepage/un-authenticated-homepage.js
+++ app/pages/homepage/un-authenticated-homepage.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import UnAuthenticatedHeader from '../un-authenticated-header'
+
 
 const UnAuthenticatedHomepage = () => {
   return (
-    <div>un-authenticated-homepage</div>
+    <>
+      <UnAuthenticatedHeader/>
+      <div className='bg-black  h-[300vh] w-full z-0'>un-authenticated-homepage</div>
+    </>
   )
 }
 
