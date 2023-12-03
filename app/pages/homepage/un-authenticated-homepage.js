6e18e766107af5e8250ed2d3ebe2e0e650897a67
Rup-Royofficial <rupayanroy981@gmail.com>
2023-12-04 03:17:22 +0530

    day6: changes, 1st segment changes, animated text etc etc

--- app/pages/homepage/un-authenticated-homepage.js
+++ app/pages/homepage/un-authenticated-homepage.js
@@ -1,85 +1,67 @@
 "use client"
 import React, { useEffect, useRef } from 'react'
 import UnAuthenticatedHeader from '../un-authenticated-header'
-import gsap from 'gsap'
-import ScrollTrigger from 'gsap/ScrollTrigger'
-import BentoGrid from '@/app/Components/BentoGrid'
+import Image from 'next/image'
 
-gsap.registerPlugin(ScrollTrigger)
+
 
 const UnAuthenticatedHomepage = () => {
-  const secondSet = useRef(null)
-  useEffect(() => {
-    if(secondSet.current){
-      gsap.to(secondSet.current, {
-        backgroundColor: "rgba(9,17,35,0.9)",
-        scrollTrigger:{
-          // trigger: secondSet.current,
-          scroller:"body",
-          start: "top -45%",
-          end: "top -80%",
-          scrub: 1,
-        }
-      })
-    }
-  }, [])
-  
 
-
+  // const secondSet = useRef(null)
+  // useEffect(() => {
+  //   if(secondSet.current){
+  //     gsap.to(secondSet.current, {
+  //       backgroundColor: "rgba(9,17,35,0.9)",
+  //       scrollTrigger:{
+  //         // trigger: secondSet.current,
+  //         scroller:"body",
+  //         start: "top 0%",
+  //         // end: "top -80%",
+  //         scrub: 1,
+  //       }
+  //     })
+  //   }
+  // }, [])
 
   return (
     <>
       <UnAuthenticatedHeader/>
       <div>
-      {/* //try out h-full too */}
-        <div className='bg-gradient-to-b from-gray-400 via-gray-900 to-black relative w-full h-[100vh]'> 
-          <img src='/images/basehomepagenebula.png' className='w-full h-full object-cover absolute mix-blend-overlay'></img>
+        {/* [box-shadow:inset_0_0_0_2000px_rgba(0,0,0,.5)] */}
+        <div className='flex align-center justify-left w-full h-screen object-cover bg-fixed bg-cover bg-center custom-img'>
+          <div className='absolute top-0 right-0 bottom-0 left-0 bg-black/50 z-[2]'></div>
+          <div className='gradient-text p-5 ml-[5rem] mt-[17rem] text-center  text-transparent z-[2] animate-gradient'>
+              {/* https://www.youtube.com/watch?v=qOkwf7VoHbM  animated text gradient*/  }
+            <h2 className='text-9xl font-bold '>Develop.Think.Deploy</h2>
+            {/* <h4 className='text-2xl mt-3'>Lets build together</h4> */}
+          </div>
+          
+        {/* //try out h-full too */}
+        </div>
+        
+        <div className='bg-green-400 h-[150vh] w-full z-10'>
+        </div>
+        <div className='bg-purple-400 h-[150vh] w-full z-10'>
+        </div>
+        <div className='bg-orange-400 h-[150vh] w-full z-10'>
+        </div>
+      </div>
+    </>
+    
+  )
+}
+
+
+export default UnAuthenticatedHomepage
+
+{/* <div className='bg-gradient-to-b from-gray-400 via-gray-900 to-black relative w-full h-[100vh]'> 
+          <img 
+            src={Mail}
+            priority={true}
+            className='w-full h-full object-cover absolute mix-blend-overlay'
+            alt='homepage-image'></img>
           <div className='p-24 mt-50'>
             <h2 className='text-slate-300 text-8xl mt-[20vw] font-bold font-[-apple-system,BlinkMacSystemFont,"Segoe_UI","Noto_Sans",Helvetica,Arial,sans-serif,"Apple_Color_Emoji","Segoe_UI_Emoji"]'>Let's build together</h2>
             <h4 className='text-slate-300 text-5xl font-light font-[-apple-system,BlinkMacSystemFont,"Segoe_UI","Noto_Sans",Helvetica,Arial,sans-serif,"Apple_Color_Emoji","Segoe_UI_Emoji"]'>lorem ipsum</h4>
           </div>
-        </div>
-
-        {
-          // {/* <div></div> */}
-          // {/* <div className='flex-grow z-2 h-screen ' style={{ background: 'linear-gradient(to right, rgb(4,17,31), rgba(4,17,31,0.8))' }}></div> */}
-          // {/* <div className='flex-grow z-2 h-screen ' style={{ background: 'rgba(9,17,35,0.9)' }}></div>
-          // <div 
-          //   className='flex-grow-0 text-[snow] w-[50%] object-cover z-3 bg-cover bg-center  opacity-1'
-          //   style={{ backgroundImage: `url('/images/basehomepagenebula.png')`}}
-          // >
-            
-          // //  <div className='inset-0 z-4 bg-black opacity-25'></div> 
-          // </div> */}
-        }
-
-      </div>
-      
-      <div 
-        className='bg-black h-[150vh] w-full z-10'
-        ref={secondSet}>
-          {/* <BentoGrid/> */}
-        </div>
-    </>
-  )
-}
-
-{/* <>
-      <UnAuthenticatedHeader/>
-      <div className='flex '>
-        <div className='flex-grow z-2 h-screen bg-[rgb(4,17,31)] opacity-[20]'></div>
-        <div 
-          className='flex-grow-0 text-[snow] w-[50%] object-cover -z-1 bg-cover bg-center '
-          style={{ backgroundImage: `url('/images/basehomepagenebula.png')`}}
-        >
-          
-        </div>
-        <div className='absolute inset-0 z-3 bg-black opacity-75'></div>
-      </div>
-      
-      <div 
-        className='bg-black h-[150vh] w-full z-10'
-        ref={secondSet}>a</div>
-    </> */}
-
-export default UnAuthenticatedHomepage
+        </div> */}
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,21 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      keyframes: {
+        gradient:{
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          },
+        },
+      },
+      animation : {
+        gradient: "gradient 10s ease infinite",
+      },
     },
   },
   plugins: [],
