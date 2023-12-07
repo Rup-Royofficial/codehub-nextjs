6939ace710272430ece96b370425b6dd83a390c7
Rup-Royofficial <rupayanroy981@gmail.com>
2023-12-07 08:30:51 +0530

    day8 changes, massive chnages on the home/landing page, some minor finishing touches left

--- app/Components/BouncyCardFeatures.jsx
+++ app/Components/BouncyCardFeatures.jsx
@@ -3,52 +3,52 @@ import { motion } from "framer-motion";
 
 export const BouncyCardsFeatures = () => {
   return (
-    <section className="mx-auto max-w-7xl px-4 py-12 text-slate-800">
+    <section className="mx-auto max-w-7xl px-4 py-12 text-slate-800 ">
       <div className="mb-8 flex flex-col items-start justify-between gap-4 md:flex-row md:items-end md:px-8">
-        <h2 className="max-w-lg text-4xl font-bold md:text-5xl">
+        <h2 className="max-w-lg text-4xl text-[snow] font-bold md:text-5xl">
           Grow faster with our
-          <span className="text-slate-400"> all in one solution</span>
+          <span className="text-slate-500"> all in one solution</span>
         </h2>
         <motion.button
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.95 }}
-          className="whitespace-nowrap rounded-lg bg-slate-900 px-4 py-2 font-medium text-white shadow-xl transition-colors hover:bg-slate-700"
+          className="whitespace-nowrap rounded-lg bg-black px-4 py-2 font-medium text-white shadow-xl transition-colors hover:bg-slate-700"
         >
           Learn more
         </motion.button>
       </div>
       <div className="mb-4 grid grid-cols-12 gap-4">
         <BounceCard className="col-span-12 md:col-span-4">
-          <CardTitle>Do a thing</CardTitle>
-          <div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-violet-400 to-indigo-400 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
+          <CardTitle>Use our AI-Bot</CardTitle>
+          <div className="bg-cover bg-start illustrationDepend-img absolute bottom-0 left-4 right-1 top-3 translate-y-8 rounded-t-2xl bg-gradient-to-br from-violet-400 to-indigo-400 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
             <span className="block text-center font-semibold text-indigo-50">
-              FEATURE DEMO HERE
+              {/* Can wriote the title of the pic or similar */}
             </span>
           </div>
         </BounceCard>
         <BounceCard className="col-span-12 md:col-span-8">
-          <CardTitle>Do another thing</CardTitle>
-          <div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-amber-400 to-orange-400 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
+          <CardTitle>Fix your workflow</CardTitle>
+          <div className="bg-cover bg-start illustrationAction-img absolute -bottom-1 left-4 -right-1 top-3 translate-y-8 rounded-t-2xl bg-gradient-to-br from-amber-400 to-orange-400 p-4 transition-transform duration-[250ms] group-hover:translate-y-2 group-hover:rotate-[2deg]">
             <span className="block text-center font-semibold text-orange-50">
-              FEATURE DEMO HERE
+              {/* Can wriote the title of the pic or similar */}
             </span>
           </div>
         </BounceCard>
       </div>
       <div className="grid grid-cols-12 gap-4">
         <BounceCard className="col-span-12 md:col-span-8">
-          <CardTitle>And this too</CardTitle>
-          <div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-green-400 to-emerald-400 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
+          <CardTitle>Try our code-editor</CardTitle>
+          <div className="bg-cover bg-start illustrationCodespace-img absolute -bottom-1 left-4 -right-1 top-3 translate-y-8 rounded-t-2xl bg-gradient-to-br from-green-400 to-emerald-400 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
             <span className="block text-center font-semibold text-emerald-50">
-              FEATURE DEMO HERE
+              {/* Can wriote the title of the pic or similar */}
             </span>
           </div>
         </BounceCard>
         <BounceCard className="col-span-12 md:col-span-4">
-          <CardTitle>And finally this</CardTitle>
-          <div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-pink-400 to-red-400 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
+          <CardTitle>Private file scan</CardTitle>
+          <div className="bg-cover bg-left illustrationSecretScan-img absolute bottom-0 left-4 right-4 top-8 translate-y-8 rounded-t-2xl bg-gradient-to-br from-pink-400 to-red-400 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
             <span className="block text-center font-semibold text-red-50">
-              FEATURE DEMO HERE
+              {/* Can wriote the title of the pic or similar */}
             </span>
           </div>
         </BounceCard>
