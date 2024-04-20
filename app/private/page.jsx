c97b873f85e8b913bf233e7ca91ce009614c1321
Rup-Royofficial <rupayanroy981@gmail.com>
2024-04-20 13:58:19 +0530

    not sure how many pointless chnages need to be done

--- app/private/page.jsx
+++ app/private/page.jsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation'
 
-import { createClient } from '@/app/utils/supabase/server'
+import { createClient } from 'app/utils/supabase/server'
 
 export default async function PrivatePage() {
   const supabase = createClient()
